ab9addd0dcc26f6aa72e38eb7410368bba5d7058
giuscri <giuscri@gmail.com>
2014-02-02 16:09:41 +0100

    fixing increment by one procedure

--- BinaryCalculator.cpp
+++ BinaryCalculator.cpp
@@ -17,31 +17,41 @@ BinaryCalculator::BinaryCalculator(int* pins)
 
 void BinaryCalculator::incrementByOne()
 {
-
-    int i;
-
-    // It has to be fixed!
-
-    for (i = 0; i < sizeof pins -1; i++)
-        if (pins[i] != true) {
-	    digitalWrite(pins[i], HIGH);
-	    pins[i] = true;
-	    return;
-	}
-	
-    if (i == sizeof pins -1) {
-        
-	if (pins[i] != true) {
-	    digitalWrite(pins[i], HIGH);
-	    pins[i] = true;
-	    return;
-	} else {
-	    for (; i >= 0; i--) {
-	        digitalWrite(pins[i], LOW);
-		pins[i] = false;
-	    }
-	}
-	
+ 
+    // If bit at pos #0 is off, flip
+    // it on, then return ...
+    if (pins[0] != true) {
+        digitalWrite(pins[0] = true;
+	pins[0] = true;
+	return;
     }
+    
+    // Else, search for the first bit that's
+    // off, then set i at that position ...
+    int i;
+    for (i = 1; i < sizeof pins && pins[i++] != false;);
+    
+    // If the for-block above's been broken by
+    // the occurrence of a bit that's free,
+    // flip it on then return ...
+    if (pins[i] != true) {
+        digitalWrite(pins[i], HIGH);
+	pins[i] = true;
+	return;
+    }
+    
+    // Else, if the for-block above's been broken
+    // by i to be beyond the last position available
+    // flip every bit off, setting the calculator
+    // at zero ...
+    else {
+        for (; i >= 0; i--) {
+	    digitalWrite(pins[i], LOW);
+	    pins[i] = false;
+	    return;
+	}
+    }
+    
+    
      
 }
