b5b648b309ff7de1c312c045ab84413994fed32e
giuscri <giuscri@gmail.com>
2014-02-08 17:59:13 +0100

    Update CountingViaLeds.ino

--- examples/CountingViaLeds/CountingViaLeds.ino
+++ examples/CountingViaLeds/CountingViaLeds.ino
@@ -7,7 +7,7 @@
 // results at led's wired at pins
 // 3, 4, 5, 6 and 7 ...
 int pins[] = {3, 4, 5, 6, 7};
-BinaryCalculator b(pins);
+BinaryCalculator b(pins, 5);
 
 void setup() {
     // Nothing to setup
